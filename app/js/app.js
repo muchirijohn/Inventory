eed362e3a698646349ad432009e1f4b472268d55
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-03-02 13:32:53 +0300

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1177,7 +1177,7 @@ const internal = require('stream');
          */
         function initHeights() {
             //log table height
-            $('.dev-desc .part-extra-info').css('height', ($(window).height() - 134) + 'px');
+            $('.dev-desc .part-extra-info').css('height', ($(window).height() - 138) + 'px');
             $('.log-table tbody').css('height', ($(window).height() - 175) + 'px');
         }
         /**
