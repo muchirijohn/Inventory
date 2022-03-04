78d8e0ab0fb47f942b24ea3846c70f56d6f7c97e
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-03-04 18:45:41 +0300

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -859,7 +859,7 @@ const internal = require('stream');
                 //stock
                 if (partsShowJson.stock == 0) slv = 2;
                 else if (filterInt(partsShowJson.stock) < filterInt(partsShowJson.stock_limit)) slv = 1;
-                pElShow.inStock.html(`<span style="color:#${stock[slv][1]};animation:${(slv === 2) ? 'text-flicker 1s infinite alternate' : 'none'}">${stock[slv][0]}</span>`);
+                pElShow.inStock.html(`<span style="color:#${stock[slv][1]};animation:${(slv === 2) ? 'text-flicker 0.5s infinite alternate' : 'none'}">${stock[slv][0]}</span>`);
                 pElShow.stock.html(`Stock : <i class="cart arrow down icon" style="color: #47ff56"></i>${partsShowJson.stock}&nbsp;&nbsp;
                                         <i class="dollar icon" style="color: #ff2335"></i>${partsShowJson.cost}`);
                 if (tb_update === true) {
