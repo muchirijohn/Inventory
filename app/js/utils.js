483b99f098da416884cf4730534f58e884ecfcd1
MuchiriJohn <muchirijohn@users.noreply.github.com>
2022-04-07 06:19:19 +0300

    Update utils.js

--- app/js/utils.js
+++ app/js/utils.js
@@ -1,6 +1,11 @@
 
 module.exports = {
 
+    /**
+     * Check if a number is an integer
+     * @param {Integer} value 
+     * @returns filtered value
+     */
     filterInt: (value) => {
         if (/^[-+]?(\d+|Infinity)$/.test(value)) {
             return Number(value)
